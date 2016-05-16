ca4dbffbf07caf7bc849aa3c992d4b7218abc655
Ge Yang <yangge1987@gmail.com>
2016-05-15 21:46:21 -0500

    add find_packages to setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='Instruments',
-      version='0.5.1',
+      version='0.6.0',
       description='A Collection of instrument drivers for scientific experiments with Python',
       author='Ge Yang',
       author_email='yangge1987@gmail.com',
       url='https://www.github.com/SchusterLab/instruments',
-      packages=['instruments'],
+      packages=find_packages(),
       download_url='https://github.com/SchusterLab/instruments/tarball/0.5.0',
       keywords=['Scientific Experimentation', 'instrumentation', 'drivers'],
       classifiers=[]
